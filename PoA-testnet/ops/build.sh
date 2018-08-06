8fa44ed1769150cbe38496fb05eb9d3bd2f1baff
Sebastian Bolanos <sebasheston@gmail.com>
2018-08-06 01:15:15 -0700

    Update deploy script

--- PoA-testnet/ops/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-git clone https://github.com/paritytech/parity-ethereum /tmp/parity
-
-docker build -f docker/alpine/Dockerfile --tag parity/builder
--- PoA-testnet/parity-poa.sh
+++ PoA-testnet/parity-poa.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 docker pull parity/parity:beta
 docker service create \
-  --name "parity" \
+  --name "parity-poa" \
   --mode "global" \
   --detach \
   --publish "31245:31245" \
-  parity/parity:beta \
+  sebohe/parity-poa \
   --jsonrpc-port=31245 \
   --jsonrpc-interface=0.0.0.0 \
   --jsonrpc-apis=all \
   --no-ws  \
   --base-path /root/parity \
   --cache-size=8192
-
 
 docker service logs -f parity --raw
